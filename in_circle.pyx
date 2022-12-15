60eb511fce2166798d5b8c3169633577df5d6749
Angel Carmen Cruzatti <angelcarmen0302@gmail.com>
2022-12-15 03:16:37 -0500

    Update in_circle.pyx

--- in_circle.pyx
+++ in_circle.pyx
@@ -6,8 +6,11 @@ def in_circle( int Cent_x,  int Cent_y , int coord_x,  int coord_y,  int radio):
     cdef  int Cx = coord_x
     cdef  int Cy = coord_y
     cdef  int r = radio
-    cdef  bool result =  ((Cx-x)**2 + (Cy-y)**2) <= (r**2)
-    return result
+   # cdef  bool result =  ((Cx-x)**2 + (Cy-y)**2) <= (r**2)
+    #return result
+    if ((Cx-x)**2 + (Cy-y)**2) <= (r**2):
+        return 1
+    return 0
 
 
 def test():
@@ -20,14 +23,19 @@ def draw_canvas( unsigned int width, unsigned int height ):
     cdef unsigned int h = height
     cdef unsigned int size = w*h
 
-    cdef  array.array red = array.array('i', [])
-    cdef  array.array green = array.array('i', [])
-    cdef  array.array blue = array.array('i', [])
+    cdef  array.array template_array = array.array('i', [])
+    cdef array.array red
+    cdef  array.array green
+    cdef  array.array blue
+    red= array.clone(template_array, size, zero=False)
+    green= array.clone(template_array, size, zero=False)
+    blue= array.clone(template_array, size, zero=False)
 
     for i in range(size):
-        red.append(0)
-        green.append(0)
-        blue.append(0)
+       red.data.as_ints[i]=0
+       green.data.as_ints[i]=0
+       blue.data.as_ints[i]=0
+    
     
     return [red, green, blue]
 
