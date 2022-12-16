0bd5705bdb887be4c72102645b3fdf1469acd337
Angel Carmen Cruzatti <angelcarmen0302@gmail.com>
2022-12-16 01:31:21 -0500

    parallel

--- setup.py
+++ setup.py
@@ -1,5 +1,17 @@
-import distutils.core
-import Cython.Build
-distutils.core.setup(
-    ext_modules = Cython.Build.cythonize("in_circle.pyx")
+from setuptools import Extension, setup
+from Cython.Build import cythonize
+
+ext_modules = [
+    Extension(
+        "in_circle_opt_parallel",
+        ["in_circle_opt_parallel.pyx"],
+        extra_compile_args=['-fopenmp'],
+        extra_link_args=['-fopenmp'],
+    )
+]
+
+setup(
+    name='parallel_circle_opt',
+    ext_modules=cythonize(ext_modules),
 )
+
