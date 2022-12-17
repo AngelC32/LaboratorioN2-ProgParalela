2394ed45620245a6d5481a2d441a559ba426acc2
altoma123 <tokkto.jr@hotmail.com>
2022-12-17 17:56:19 -0500

    Comentarios añadidos al codigo Python

--- Circulos_Cython_Version/circulos_mod.py
+++ Circulos_Cython_Version/circulos_mod.py
@@ -1,16 +1,23 @@
 import array 
 import math
 from draw_processing import draw_canvas,draw_circle
-import time
-
+#import time
 
 def writePPM(red, green, blue, width, height, filename):
+    '''
+        Pinta y guarda una imagen segun un nombre de archivo dado en 
+        formato ppm según un largo, ancho y 3 canales de color.
+        
+        Nota: La función presente fue distribuida por el docente del
+              curso y modificada por los integrantes del grupo.
+    '''
+
     ppm_header = f'P6 {width} {height} {255}\n'
     rgb = []
     for i in range(len(red)):
-        rgb.append(red[i]) # Red 
-        rgb.append(green[i]) # Green 
-        rgb.append(blue[i]) # Blue
+        rgb.append(red[i])      # Red 
+        rgb.append(green[i])    # Green 
+        rgb.append(blue[i])     # Blue
     image = array.array('B', rgb)
 
     with open(filename + '.ppm', 'wb') as f:
@@ -23,21 +30,23 @@ if __name__ == "__main__":
 
     width = 1024
     height = 960
-    cuadro =  draw_canvas(width, height)
+    cuadro =  draw_canvas(width, height)    #Funcion Modulo Cython
 
-    start = time.time()
+    #start = time.time()
 
+    # Bucle para pintar cada circulo uno tras otro
     for i in range(n_circulos):
         
+        #Lectura de datos
         linea=input.readline()
         circulo=linea[:len(linea)-1].split(" ")
+
         circulo_int=[int(i) for i in circulo]
-        draw_circle(array.array("i",circulo_int),cuadro,width,height)
+        draw_circle(array.array("i",circulo_int),cuadro,width,height)  #Funcion Modulo Cython
 
-    end = time.time()
-    elapsed = end - start
-
-    print(elapsed)
+    #end = time.time()
+    #elapsed = end - start
+    #print(elapsed)
 
     writePPM(cuadro[0], cuadro[1], cuadro[2], width, height, "salida")
     
--- Circulos_Python_Version/circulos.py
+++ Circulos_Python_Version/circulos.py
@@ -1,15 +1,23 @@
 import array 
 import math
 import os
-import time
+#import time
 
 def writePPM(red, green, blue, width, height, filename):
+    '''
+        Pinta y guarda una imagen segun un nombre de archivo dado en 
+        formato ppm según un largo, ancho y 3 canales de color.
+        
+        Nota: La función presente fue distribuida por el docente del
+              curso y modificada por los integrantes del grupo.
+    '''
+
     ppm_header = f'P6 {width} {height} {255}\n'
     rgb = []
     for i in range(len(red)):
-        rgb.append(red[i]) # Red 
-        rgb.append(green[i]) # Green 
-        rgb.append(blue[i]) # Blue
+        rgb.append(red[i])      # Red 
+        rgb.append(green[i])    # Green 
+        rgb.append(blue[i])     # Blue
     image = array.array('B', rgb)
 
     with open(filename + '.ppm', 'wb') as f:
@@ -17,12 +25,29 @@ def writePPM(red, green, blue, width, height, filename):
         image.tofile(f)
 
 def comprobar_pixel(oldPixel,newPixel):
+    '''
+        Aplica una operación XOR (OR exclusiva) a 2 datos y devuelve
+        el resultado.
+    '''
+
     return oldPixel ^ newPixel
 
 def in_circle(Cx,Cy,x,y,r):
+    '''
+        Verifica y devuelve True o False si los puntos "x" e "y" se 
+        encuentran dentro de la circunferencia de ecuación:
+
+                    (Cx - x)^2 + (Cy - y)^2 = r^2
+    '''
+
     return (math.pow((Cx-x),2) + math.pow(Cy-y,2)) <= (math.pow(r,2))
 
 def draw_canvas(width, height):
+    '''
+        Pinta un lienzo totalmente en negro según un alto y ancho
+        especificado y lo devuelve.
+    '''
+
     red = []
     green=[]
     blue=[]
@@ -34,6 +59,15 @@ def draw_canvas(width, height):
     return [red, green, blue]
 
 def draw_circle(circulo, cuadro, width, height):
+    '''
+        Dibuja un "circulo" dentro de un "cuadro" de alto y ancho
+        especificado y lo devuelve:
+
+        Recibe:
+            circulo: Lista con los datos del circulo [x,y,r,R,G,B]
+            cuadro: lienzo en el que se pintaran los circulos
+    '''
+
     red = cuadro[0]
     green = cuadro[1]
     blue = cuadro[2]
@@ -45,10 +79,12 @@ def draw_circle(circulo, cuadro, width, height):
     color_green=int(circulo[4])
     color_blue=int(circulo[5])
 
+    # Recorriendo el cuadro
     for i in range(height):
         for j in range(width):
             if(in_circle(eje_X,eje_Y,i,j,radio)):
                 index = j+i*width
+
                 red[index]=comprobar_pixel(red[index],color_red)
                 green[index]=comprobar_pixel(green[index],color_green)
                 blue[index]=comprobar_pixel(blue[index],color_blue)
@@ -63,19 +99,19 @@ if __name__ == "__main__":
     height = 960
     cuadro = draw_canvas(width, height)
 
-    start = time.time()
+    #start = time.time()
 
+    # Bucle para pintar cada circulo uno tras otro
     for i in range(n_circulos):
-        
+        #Lectura de datos
         linea=input.readline()
         circulo=linea[:len(linea)-1].split(" ")
         
         draw_circle(circulo, cuadro, width, height)
 
-    end = time.time()
-    elapsed = end - start
-
-    print(elapsed)
+    #end = time.time()
+    #elapsed = end - start
+    #print(elapsed)
 
     writePPM(cuadro[0], cuadro[1], cuadro[2], width, height, "salida")
 
